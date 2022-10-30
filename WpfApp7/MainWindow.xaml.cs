bc431d8d4ce3cfc0f561902da92100ee1c8c90c3
user <user@WINDOWS-OM6HICP>
2022-10-30 17:01:30 +0400

    Add project files.

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using Bogus;
+
+namespace WpfApp7;
+
+#nullable disable
+
+public partial class MainWindow : Window
+{
+	public List<User> Users { get; set; }
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		DataContext = this;
+
+		Users = new Faker<User>()
+			.RuleFor(u => u.Firstname, f => f.Person.FirstName)
+			.RuleFor(u => u.Lastname, f => f.Person.LastName)
+			.RuleFor(u => u.Gender, f => f.Person.Gender.ToString())
+			.RuleFor(u => u.Phone, f => f.Person.Phone)
+			.RuleFor(u => u.ImageUrl, f => f.Person.Avatar)
+			.RuleFor(u => u.Email, f => f.Person.Email)
+			.RuleFor(u => u.Address, f => f.Person.Address.City)
+			.RuleFor(u => u.DateOfBirth, f => f.Person.DateOfBirth)
+			.Generate(10);
+
+		//Users.Add(new User { Firstname = "HuseynNuran" });
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Button btn = sender as Button;
+		Grid grid = btn.Parent as Grid;
+		StackPanel sp = grid.Children[1] as StackPanel;
+		TextBlock tb = sp.Children[0] as TextBlock;
+
+		MessageBox.Show(tb.Text);
+	}
+}
---
+++ WpfApp7/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WpfApp7;
+
+#nullable disable
+
+public class User
+{
+	public Guid Id { get; set; } = Guid.NewGuid();
+	public string Firstname { get; set; }
+	public string Lastname { get; set; }
+	public string Gender { get; set; }
+	public string Phone { get; set; }
+	public string ImageUrl { get; set; }
+	public string Email { get; set; }
+	public string Address { get; set; }
+	public DateTime DateOfBirth { get; set; }
+}
